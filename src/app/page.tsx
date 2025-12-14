2744df3295414deb40af44e64c739b0fcfe68d66
Marios Kanellopoulos <mariosknl@outlook.com>
2025-12-14 10:21:43 +0200

    Add @upstash/redis dependency, implement room creation API, and update Home component to handle room creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { client } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ANIMALS = [
@@ -43,6 +46,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const main = () => {
@@ -60,6 +64,16 @@ export default function Home() {
 
     main();
   }, []);
+
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post();
+
+      if (res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`);
+      }
+    },
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
@@ -86,7 +100,10 @@ export default function Home() {
               </div>
             </div>
 
-            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
+            <button
+              onClick={() => createRoom()}
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+            >
               CREATE SECURE ROOM
             </button>
           </div>
